1a2f5d437612504647f5ff8c82b74b299c75055c
TruongThuan <thuan20202000@gmail.com>
2020-04-06 00:24:51 +0700

    Finished Edit and continue to OrderHistory UI

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View,Text,StyleSheet} from 'react-native';
 
 import ShopNavigator from './navigation/navigator';
@@ -9,18 +9,24 @@ import { createStore, combineReducers,applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import productsReducer from './store/reducer/products';
 import cartReducer from './store/reducer/cart';
+import categoriesReducer from './store/reducer/category';
+import authenticationReducer from './store/reducer/users';
 import ReduxThunk from 'redux-thunk';
 
 
 const rootReducer = combineReducers({
   products: productsReducer,
-  cartItems : cartReducer
+  cartItems : cartReducer,
+  categories : categoriesReducer,
+  authentication : authenticationReducer,
+
 })
 const store = createStore(rootReducer,applyMiddleware(ReduxThunk));
 
 
 const App = () =>{
   
+
   return (
     <Provider store={store}>
           <ShopNavigator  />
